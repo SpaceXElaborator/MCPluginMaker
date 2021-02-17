9ce1a1dba6b0e89166e69a3380519056579a1974
Sean Rahman <terturlminecraft@gmail.com>
2021-02-17 12:40:52 -0800

    Java code for custom items was added

--- Templates.go
+++ Templates.go
@@ -11,12 +11,60 @@ import com.{{.Author}}.net.cmds.*;{{end}}
 
 public class Main extends JavaPlugin {
 
+	private static Main instance;
+
 	public void onEnable() {
+		instance = this;
 	{{with .Cmds -}}{{range $val := .}}	getCommand("{{$val.SlashCommand}}").setExecutor(new {{$val.Name}}());
 	{{end}}{{end}}
+	
+		{{.Name}}CustomItems.RegisterItems();
+	}
+	
+	public static Main getInstance() {
+		return instance;
 	}
 
 }
+`
+
+// Creates ALL the custom items that the user wants
+itemsJavaTmpl = `package com.{{.Author}}.net;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class {{.Name}}CustomItems {
+	public static List<ItemStack> items = new ArrayList<ItemStack>();
+	
+	public static void RegisterItems() {
+		{{with .Items -}}{{range $val := .}}
+		ItemStack {{$val.ItemName}}Item = new ItemStack(Material.valueOf("{{$val.ItemMaterial}}"));
+		ItemMeta {{$val.ItemName}}Meta = {{$val.ItemName}}Item.getItemMeta();
+		{{$val.ItemName}}Meta.setDisplayName("{{$val.ItemName}}");
+		List<String> {{$val.ItemName}}Lore = new ArrayList<String>();
+		{{with $val.ItemDescription -}}{{range $strings := .}}{{$val.ItemName}}Lore.add("{{.}}");
+		{{end}}{{end}}{{$val.ItemName}}Meta.setLore({{$val.ItemName}}Lore);
+		{{$val.ItemName}}Item.setItemMeta({{$val.ItemName}}Meta);
+		items.add({{$val.ItemName}}Item);
+		{{end}}{{end}}
+	}
+	
+	public static ItemStack getItem(String name) {
+		for(ItemStack item : items) {
+			if(item.getItemMeta().getDisplayName().equalsIgnoreCase(name)) {
+				return item;
+			}
+		}
+		return null;
+	}
+	
+}
+
 `
 
 // Create commands that will here soon have more functionality
