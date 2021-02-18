6c1aa0773cf1140ca011429f6ec0e6fd58c7eb8a
Sean Rahman <terturlminecraft@gmail.com>
2021-02-18 13:31:29 -0800

    Changed TableView to instead work with a GridBlockLayout for a cleaner look

--- MainWindow.go
+++ MainWindow.go
@@ -12,18 +12,10 @@ import (
 var (
 	// Create the global variables needed
 	modal *widget.PopUp
-	secondModal *widget.PopUp
 	a fyne.App = app.New()
 	w fyne.Window = a.NewWindow("MCPluginMaker | " + GetAuthor())
 	
 	// -------------- Buttons --------------
-	addCmdButt = widget.NewButton("Add Command", func() {
-		commandForm := createCommandForm()
-		modal = widget.NewModalPopUp(commandForm, w.Canvas())
-		modal.Resize(fyne.NewSize(512, 0))
-		modal.Show()
-	})
-	
 	addItemButt = widget.NewButton("Add Item", func() {
 		itemForm := customItemForm()
 		modal = widget.NewModalPopUp(itemForm, w.Canvas())
@@ -91,21 +83,11 @@ func createToolbar() *widget.Toolbar {
 
 // Create the offical GUI using Fyne
 func ShowMainMenu() {
-	HideButtons()
-	
-	/*mainCenter := container.NewVBox(
-			addCmdButt,
-			addItemButt,
-			addBuildButt,
-	)*/
-	
 	toolbar := createToolbar()
-	
 	split := container.NewHSplit(list, container.NewVBox(
 			widget.NewLabel("Please Select A Project Or Create A New One"),
 		))
 	split.Offset = 0.1
-	
 	c := container.NewBorder(
 		toolbar,
 		nil,
@@ -113,43 +95,30 @@ func ShowMainMenu() {
 		nil,
 		split,
 	)
-	
 	list.OnSelected = func(id widget.ListItemID) {
 		CWP = PluginProjects[id].Name
 		w.SetTitle("MCPluginMaker | " + GetAuthor())
-		UnhideButtons()
 		SetNewContent()
 	}
-	
 	w.Resize(fyne.NewSize(1024, 768))
 	w.SetContent(c)
 	w.ShowAndRun()
 }
 
 func SetNewContent() {
-
-	var apps fyne.CanvasObject
-
-	if len(GetProject(CWP).CmdRows) >= 1 {
-		apps = container.NewAppTabs(
-			container.NewTabItem("Commands", 
-				container.NewBorder(
-					createCmdToolbar(),
-					nil,
-					nil,
-					nil,
-					container.NewMax(CreateCommandBlocks()),
-				),
+	apps := container.NewAppTabs(
+		container.NewTabItem("Commands", 
+			container.NewBorder(
+				createCmdToolbar(),
+				nil,
+				nil,
+				nil,
+				CreateCommandBlocksTest(),
 			),
-		)
-	} else {
-		apps = container.NewAppTabs(
-			container.NewTabItem("Commands", container.NewMax(widget.NewLabel("Create A Command To View This"))),
-		)
-	}
+		),
+	)
 	
 	card := widget.NewCard("Project: " + CWP, "", apps)
-	
 	toolbar := createToolbar()
 	split := container.NewHSplit(list, card)
 	split.Offset = 0.1
@@ -165,71 +134,25 @@ func SetNewContent() {
 	w.SetContent(c)
 }
 
-func CreateCommandBlocks() *widget.Table {
-	proj := GetProject(CWP)
-	cmdRows := proj.CmdRows
-	cmdLength := len(cmdRows[len(cmdRows) - 1].Cmds)
-	dmyCmd := Command{"", "", ""}
-	if cmdLength == 1 {
-		cmdRows[len(cmdRows) - 1].Cmds = append(cmdRows[len(cmdRows) - 1].Cmds, dmyCmd, dmyCmd)
-	}
-	if cmdLength == 2 {
-		cmdRows[len(cmdRows) - 1].Cmds = append(cmdRows[len(cmdRows) - 1].Cmds, dmyCmd)
-	}
-	table := widget.NewTable(
-		func() (int, int) {
-			return len(cmdRows), 3
-		},
-		func() fyne.CanvasObject {
-			name := widget.NewLabelWithStyle("Name Text", fyne.TextAlignCenter, fyne.TextStyle{})
-			sep := widget.NewSeparator()
-			flavor := widget.NewLabel("Flavor Text")
-			
-			con := container.NewVBox(
-				name,
-				sep,
-				flavor,
-			)
-			return con
-		},
-		func(tci widget.TableCellID, f fyne.CanvasObject) {
-			f.(*fyne.Container).Objects[0].(*widget.Label).SetText(cmdRows[tci.Row].Cmds[tci.Col].Name)
-			if cmdRows[tci.Row].Cmds[tci.Col].SlashCommand == "" {
-				f.(*fyne.Container).Objects[2].(*widget.Label).SetText("")
-			} else {
-				f.(*fyne.Container).Objects[2].(*widget.Label).SetText("/" + cmdRows[tci.Row].Cmds[tci.Col].SlashCommand)
-			}
-			
-		})
+func CreateCommandBlocksTest() fyne.CanvasObject {
+	var test []fyne.CanvasObject
 	
-	table.SetColumnWidth(0, 270)
-	table.SetColumnWidth(1, 270)
-	table.SetColumnWidth(2, 270)
+	for _, f := range GetProject(CWP).Cmds {
+		card := widget.NewCard(
+			f.Name,
+			f.SlashCommand,
+			nil,
+		)
+		test = append(test, card)
+	}
 	
-	return table
-}
-
-// A function to quickly disable all buttons
-func HideButtons() {
-	addCmdButt.Disable()
-	addItemButt.Disable()
-	addBuildButt.Disable()
-}
-
-// A function to quickly enable all buttons
-func UnhideButtons() {
-	addCmdButt.Enable()
-	addItemButt.Enable()
-	addBuildButt.Enable()
+	content := container.NewVScroll(container.NewGridWithColumns(3, test...))
+	return content
 }
 
 // Get the current modal being displayed and hide it if it isn't
 func HideModal() {
 	modal.Hide()
-}
-
-func HideModal2() {
-	secondModal.Hide()
 }
 
 func GetApp() fyne.App {
