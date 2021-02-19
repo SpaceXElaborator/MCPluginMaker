1e82429f094f912da4052903069fc8ecba983a15
Sean Rahman <terturlminecraft@gmail.com>
2021-02-19 08:37:01 -0800

    Made sure you can't create two of the same items with item name

--- CustomItem.go
+++ CustomItem.go
@@ -9,6 +9,16 @@ import (
 	"fyne.io/fyne/v2/dialog"
 )
 
+func CheckItem(s string) bool {
+	proj := GetProject(CWP)
+	for _, f := range proj.Items {
+		if strings.ToLower(s) == strings.ToLower(f.ItemName) {
+			return true
+		}
+	}
+	return false
+}
+
 func customItemForm() *widget.Form {
 
 	itemNameEntry := widget.NewEntry()
@@ -43,26 +53,30 @@ func customItemForm() *widget.Form {
 	newCustomItemForm := widget.NewForm(itemNameFormItem, itemMaterialFormItem, itemDescFormItem)
 	newCustomItemForm.OnSubmit = func() {
 		if itemNameEntry.Text != "" && itemMaterialEntry.Text != "" {
-			stringToCheck := strings.ReplaceAll(strings.ToUpper(itemMaterialEntry.Text), " ", "_")
-			if(CheckMaterial(stringToCheck)) {
-				newItem := CustomItem{GetAuthor(), stringToCheck, itemNameEntry.Text, strings.Split(itemDescEntry.Text, "\n")}
-			
-				proj := GetProject(CWP)
-				proj.Items = append(proj.Items, newItem)
-			
-				// Reset the project to be the new proj pointer
-				var index int
-				for i, cmd := range PluginProjects {
-					if proj.Name == cmd.Name {
-						index = i
-					}
-				}
-				PluginProjects[index] = *proj
+			if CheckItem(itemNameEntry.Text) != true {
+				stringToCheck := strings.ReplaceAll(strings.ToUpper(itemMaterialEntry.Text), " ", "_")
+				if(CheckMaterial(stringToCheck)) {
+					newItem := CustomItem{GetAuthor(), stringToCheck, itemNameEntry.Text, strings.Split(itemDescEntry.Text, "\n")}
 				
-				// HideModal() in MainWindow.go
-				HideModal()
+					proj := GetProject(CWP)
+					proj.Items = append(proj.Items, newItem)
+				
+					// Reset the project to be the new proj pointer
+					var index int
+					for i, cmd := range PluginProjects {
+						if proj.Name == cmd.Name {
+							index = i
+						}
+					}
+					PluginProjects[index] = *proj
+					
+					// HideModal() in MainWindow.go
+					HideModal()
+				} else {
+					dialog.ShowError(errors.New("Unrecognized Material! Check https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Material.html"), GetWindow())
+				}
 			} else {
-				dialog.ShowError(errors.New("Unrecognized Material! Check https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Material.html"), GetWindow())
+				dialog.ShowError(errors.New("An item with that name exists"), GetWindow())
 			}
 		} else {
 			dialog.ShowError(errors.New("Item Name and Item Material can't be empty"), GetWindow())
