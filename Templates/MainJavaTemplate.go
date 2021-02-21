23a2d636090b26b5ce59b00d8d176f106e34de29
Sean Rahman <terturlminecraft@gmail.com>
2021-02-20 23:03:27 -0800

    Fixed mis-copy of string where Main.java would create a CustomItem class instead of the main class

--- Templates/MainJavaTemplate.go
+++ Templates/MainJavaTemplate.go
@@ -3,39 +3,26 @@ package PluginTemplates
 func GetMainClassTemplate() string {
 	return `package com.{{.Author}}.net;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.bukkit.plugin.java.JavaPlugin;
+{{if .Cmds}}
+import com.{{.Author}}.net.cmds.*;{{end}}
 
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+public class Main extends JavaPlugin {
 
-public class {{.Name}}CustomItems {
-	public static List<ItemStack> items = new ArrayList<ItemStack>();
+	private static Main instance;
+
+	public void onEnable() {
+		instance = this;
+	{{with .Cmds -}}{{range $val := .}}	getCommand("{{$val.SlashCommand}}").setExecutor(new {{$val.Name}}());
+	{{end}}{{end}}
 	
-	public static void RegisterItems() {
-		{{with .Items -}}{{range $val := .}}
-		// <<ITEM:{{$val.ItemMaterial}}||{{$val.ItemName}}||{{with $val.ItemDescription -}}{{range $strings := .}}{{.}}\n{{end}}{{end}}>>
-		ItemStack {{$val.ItemName}}Item = new ItemStack(Material.valueOf("{{$val.ItemMaterial}}"));
-		ItemMeta {{$val.ItemName}}Meta = {{$val.ItemName}}Item.getItemMeta();
-		{{$val.ItemName}}Meta.setDisplayName("{{$val.ItemName}}");
-		List<String> {{$val.ItemName}}Lore = new ArrayList<String>();
-		{{with $val.ItemDescription -}}{{range $strings := .}}{{$val.ItemName}}Lore.add("{{.}}");
-		{{end}}{{end}}{{$val.ItemName}}Meta.setLore({{$val.ItemName}}Lore);
-		{{$val.ItemName}}Item.setItemMeta({{$val.ItemName}}Meta);
-		items.add({{$val.ItemName}}Item);
-		{{end}}{{end}}
+		{{.Name}}CustomItems.RegisterItems();
 	}
 	
-	public static ItemStack getItem(String name) {
-		for(ItemStack item : items) {
-			if(item.getItemMeta().getDisplayName().equalsIgnoreCase(name)) {
-				return item;
-			}
-		}
-		return null;
+	public static Main getInstance() {
+		return instance;
 	}
-	
+
 }
 `
 }
