8548948756eb942acddd5d1a921efc6a17e7c837
Sean Rahman <terturlminecraft@gmail.com>
2021-02-21 08:22:19 -0800

    Moved to registering items with new Item.java util class instead of blocks of code

--- Templates/ItemTemplate.go
+++ Templates/ItemTemplate.go
@@ -4,6 +4,7 @@ func GetItemTemplate() string {
 	return `package com.{{.Author}}.net;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.Material;
@@ -11,31 +12,34 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class {{.Name}}CustomItems {
-	public static List<ItemStack> items = new ArrayList<ItemStack>();
+
+	private static List<Item> items = new ArrayList<Item>();
 	
-	public static void RegisterItems() {
+	public static void items() {
 		{{with .Items -}}{{range $val := .}}
 		// <<ITEM:{{$val.ItemMaterial}}||{{$val.ItemName}}||{{with $val.ItemDescription -}}{{range $strings := .}}{{.}}\n{{end}}{{end}}>>
-		ItemStack {{$val.ItemName}}Item = new ItemStack(Material.valueOf("{{$val.ItemMaterial}}"));
-		ItemMeta {{$val.ItemName}}Meta = {{$val.ItemName}}Item.getItemMeta();
-		{{$val.ItemName}}Meta.setDisplayName("{{$val.ItemName}}");
-		List<String> {{$val.ItemName}}Lore = new ArrayList<String>();
-		{{with $val.ItemDescription -}}{{range $strings := .}}{{$val.ItemName}}Lore.add("{{.}}");
-		{{end}}{{end}}{{$val.ItemName}}Meta.setLore({{$val.ItemName}}Lore);
-		{{$val.ItemName}}Item.setItemMeta({{$val.ItemName}}Meta);
-		items.add({{$val.ItemName}}Item);
+		RegisterItem("{{$val.ItemName}}", "{{$val.ItemMaterial}}", Arrays.asList({{with $val.ItemDescription -}}{{range $ind, $strings := .}}{{if $ind}},{{end}}"{{.}}"{{end}}{{end}}));
 		{{end}}{{end}}
 	}
 	
-	public static ItemStack getItem(String name) {
-		for(ItemStack item : items) {
-			if(item.getItemMeta().getDisplayName().equalsIgnoreCase(name)) {
-				return item;
-			}
-		}
-		return null;
+	private static void RegisterItem(String itemName, String mat, List<String> lore) {
+		items.add(new Item(itemName, mat, lore));
 	}
 	
-}
-`
+	public static ItemStack build(String name, Integer amount) {
+		Item itm = null;
+		for(Item i : items) {
+			if(i.getName().equalsIgnoreCase(name)) {
+				itm = i;
+			}
+		}
+		ItemStack item = new ItemStack(Material.valueOf(itm.getMaterial()), amount);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(itm.getName());
+		meta.setLore(itm.getLore());
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+}`
 }
