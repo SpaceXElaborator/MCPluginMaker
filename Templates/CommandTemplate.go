87977318fa30ea19bf7442fc02a1fc9032ec6480
Sean Rahman <terturlminecraft@gmail.com>
2021-02-21 13:23:04 -0800

    Added some imports to the command file, some work is still needed to be done so that it can be added/not added depending on if you have to spawn items

--- Templates/CommandTemplate.go
+++ Templates/CommandTemplate.go
@@ -8,6 +8,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 import com.{{.Author}}.net.*;
 
 public class {{.Name}} implements CommandExecutor {
